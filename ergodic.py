d8107134227cf3989305071873474184f5287392
buyaoya <77056150+buyaoya@users.noreply.github.com>
2024-05-20 22:16:06 +0800

    遍历
    遍历

--- ergodic.py
+++ ergodic.py
@@ -160,7 +160,7 @@ def get_split_idx(all_split_points, elevation_height, height, truss_height, uppe
 def splitElevation(elevations,i,split_idx):
     
     # 创建分割面
-    all_points = calculate_split_points(elevation_height, height, upper_extension_length, degree, lower_slope_length, scale_factor)
+    
     # split_idx=get_split_idx(all_points,elevation_height, height, truss_height, upper_extension_length, lower_extension_length, degree, scale_factor)
     if len(split_idx)!=0:
         split_points = [all_points[i] for i in split_idx] # 这里想一个切好几刀
@@ -499,6 +499,19 @@ def threePack(elevationSplit):
     print("是否在箱子内：",isInBox(anotherBoxmid))
 
 
+from itertools import combinations
+
+def get_splitsets(all_points):
+    # 存储所有子集的列表
+    splitsets = []
+    lst = list(range(len(all_points)))
+    # 遍历所有可能的子集大小
+    for i in range(len(lst) + 1):
+        # 生成当前大小的所有子集
+        for combo in combinations(lst, i):
+            splitsets.append(list(combo))
+    return splitsets   
+
 
 boxLength=12000
 boxWidth=2566
@@ -514,7 +527,7 @@ degree = 30
 num=1 # 梯数
 if __name__ == "__main__":
     # 初始化显示
-    display, start_display, add_menu, add_function_to_menu = init_display()
+    # display, start_display, add_menu, add_function_to_menu = init_display()
 
     boxNum=1
     boxs=[]
@@ -522,34 +535,53 @@ if __name__ == "__main__":
     
     elevations=[]
     elevations =cs(elevation_height, height, truss_height, upper_extension_length, lower_extension_length, degree, scale_factor,num)
+    all_points = calculate_split_points(elevation_height, height, upper_extension_length, degree, lower_slope_length, scale_factor)
     #每个梯子要分割的点的索引
     # split_idx = [1]
+    
     split_idx = [1]  #切三段
     split_idxs=[]
-    split_idxs.append(split_idx)
+    split_idxs=get_splitsets(all_points)[1:]  # 去掉第一个全空
+    
+    # split_idxs.append(split_idx)
     elevationSplit={}  # 记载所有扶梯的分割块 字典形式记录  0 第一个梯子
 
     
     
     # 切完
     for i in split_idxs:
+        print(i)
         split_points=splitElevation(elevations,0,i)  
         #elevationSplit[0]  一个切完所有形状的集合  0表示选择创建的第一个梯子 目前只创建一个梯子
         # 将所有形状
-        
+        preprocess_sahpes=[]
+        for bufen in elevationSplit[0]:
+            center=calculate_centroid(bufen)
+            origin = gp_Pnt(0, 0, 0)
+            move_vector = gp_Vec(center, origin)
+            # 将质心移到原点
+            transformation = gp_Trsf()
+            transformation.SetTranslation(move_vector)
+            
+            # 应用平移变换
+            transformer = BRepBuilderAPI_Transform(bufen, transformation)
+            moved_shape = transformer.Shape()
+            preprocess_sahpes.append(moved_shape)
         placedinbox=[]
         successd=[]
         res=0
-        for bufen in elevationSplit[0]:
+        for bufen in preprocess_sahpes:
             for x in range(-2000,12000,100):
+                print(1)
                 for z in range(-1000,2566,20):
                     for angle in range(0,360,3):
+                        # print(1)
                         trsf = gp_Trsf()
                         # 定义一个向量，表示沿Z轴移动
                         move_vector = gp_Vec(x, 0, z)
                         # 应用平移转换
                         trsf.SetTranslation(move_vector)
-                    
+                        
                         axis_dir = gp_Dir(0, 1, 0)  # 围绕 Y 轴
                         axis_pnt=calculate_centroid(bufen) # 计算质心
                         axis = gp_Ax1(axis_pnt, axis_dir)
@@ -568,4 +600,4 @@ if __name__ == "__main__":
                             if  not flag: # 如果未碰撞 且在箱中
                                 successd.append(Shape)
                                 res +=1
-                            
+        print("res",res) 
